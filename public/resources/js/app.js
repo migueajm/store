7187fde6e223f1b6f50f889aaeb75a98d0c941f3
MigueAJM <miguejm@outlook.es>
2024-12-26 15:34:13 -0600

    feature: span required added

--- public/resources/js/app.js
+++ public/resources/js/app.js
@@ -2,4 +2,14 @@ import { router } from './utilities/router.js';
 document.addEventListener('DOMContentLoaded', () => {
 	router.routeHandler();
 	window.addEventListener('popstate', router.routeHandler);
+	[
+		...document.querySelectorAll('input'),
+		...document.querySelectorAll('textarea'),
+		...document.querySelectorAll('select'),
+	].forEach(element => {
+		const label =  element.parentNode.querySelector(`label[for=${element.id}]`);
+		if(!label || !element.required) return;
+		const content = label.innerHTML;
+		label.innerHTML = '<span class="text-danger">* </span>' + content;
+	})
 });
